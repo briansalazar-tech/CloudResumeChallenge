60628518310629849518673939aebe5f212b0bc3
Brian Salazar <briansalazar5@gmail.com>
2024-10-30 15:26:54 -0700

    Update Resume Page View Counter

--- js/resumeviews.js
+++ js/resumeviews.js
@@ -1,19 +1,24 @@
 // Functionality used to display the current visitors on the resume page.
-
-var myStorage = window.localStorage;
 var visits;
 
-
 window.addEventListener('load', function() {
-    if (!myStorage.getItem("visits")) {
-        myStorage.setItem('visits', 1);
-    } else {
-        visits = myStorage.getItem("visits");
-        visits = parseInt(visits) + 1;
-        myStorage.setItem('visits', visits);
-    }
-    console.log('Page view count:', myStorage.getItem("visits"));
-    console.log(visits)
-});
+    fetch('https://crc-api-function.azurewebsites.net/api/HttpTrigger?count=get')
 
-document.getElementById("resumeviews").textContent = Number(myStorage.getItem("visits")) + 1;
+    .then(response => response.text())
+    .then(data => {
+        // Parse the HTML string into a DOM object
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+
+        // Get the body content
+        visits = doc.querySelector('body').innerText;
+        document.getElementById("resumeviews").textContent = visits;
+
+        console.log(visits);
+    })
+
+    // Display ERR if there are issues getting view count.
+    .catch(error => console.error('Error fetching the page:', error));
+    document.getElementById("resumeviews").textContent = "ERR";
+
+});
